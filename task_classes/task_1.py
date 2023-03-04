08dcf3e216fa1db8d56f6c12802ad7e0be712eba
Tsvetkov <zwetkow7@gmail.com>
2023-03-04 10:06:37 +0300

    classes

---
+++ task_classes/task_1.py
@@ -0,0 +1,12 @@
+class Students():
+    def __init__(self, name, surname, gender, age):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.age = age
+class Mentor():
+    def __init__(self, name, surname, experience):
+        self.name = name
+        self.surname = surname
+        self.experience = experience
+        self.courses_attached = []
