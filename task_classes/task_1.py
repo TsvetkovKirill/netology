6d71899dc99d57ef11d55f2811d7342a8e0d7e6d
Tsvetkov <zwetkow7@gmail.com>
2023-03-05 11:28:42 +0300

    первые два ДЗ

--- task_classes/task_1.py
+++ task_classes/task_1.py
@@ -1,14 +1,61 @@
-class Students():
-    def __init__(self, name, surname, gender, age):
+class Student:
+    def __init__(self, name, surname, gender):
         self.name = name
         self.surname = surname
         self.gender = gender
-        self.age = age
-class Mentor():
-    def __init__(self, name, surname, experience):
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+
+class Mentor:
+    def __init__(self, name, surname):
         self.name = name
         self.surname = surname
-        self.experience = experience
         self.courses_attached = []
-print('check')
 
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.lecture_grades = {}
+
+    def rate_lecture(self, student, course, grade):
+        if course not in self.courses_attached:
+            return 'Преподаватель не преподает данный курс'
+        if course in self.lecture_grades:
+            self.lecture_grades[course][student] = grade
+        else:
+            self.lecture_grades[course] = {student: grade}
+
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+
+cool_mentor = Reviewer('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+print(best_student.grades)
+
+lecturer = Lecturer('Mega', 'Lecturer')
+lecturer.courses_attached += ['Python']
+print(lecturer.courses_attached)
+error = lecturer.rate_lecture(best_student, 'Python', 10)
+if error:
+    print(error)
+else:
+    print(lecturer.lecture_grades)
